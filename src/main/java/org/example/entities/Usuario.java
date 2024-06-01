fa20795e95cc2f8cbe343db99bfaeeda40ded7dc
yagoluucas <yago543@gmail.com>
2024-06-01 20:41:17 -0300

    Adicionado getter e setter na entidade usuario

--- src/main/java/org/example/entities/Usuario.java
+++ src/main/java/org/example/entities/Usuario.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Usuario extends _BaseEntitie{
     private String nome;
+    private String email;
     private String senha;
     private List<Denuncia> denuncias;
 
@@ -20,5 +21,47 @@ public class Usuario extends _BaseEntitie{
         this.nome = nome;
         this.senha = senha;
         this.denuncias = denuncias;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public List<Denuncia> getDenuncias() {
+        return denuncias;
+    }
+
+    public void setDenuncias(List<Denuncia> denuncias) {
+        this.denuncias = denuncias;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", denuncias=" + denuncias +
+                "} " + super.toString();
     }
 }
